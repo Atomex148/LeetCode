03ae9373ecf984269783936731991e61b890b866
Atomex148 <unclebenis148@gmail.com>
2024-07-27 22:06:04 +0300

    Time: 0 ms (100.00%), Space: 8.1 MB (10.09%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        char* resultStr = new char[word1.length() + word2.length() + 1];
+        resultStr[word1.length() + word2.length()] = '\0';
+
+        int minLen = std::min(word1.length(), word2.length());
+
+        for (int i = 0; i < minLen; i++) {
+            resultStr[i * 2] = word1[i];
+        }
+
+        for (int i = 0; i < minLen; i++) {
+            resultStr[(i * 2) + 1] = word2[i];
+        }
+
+        if (word1.length() > minLen) {
+            for (int i = minLen; i < word1.length(); i++) {
+                resultStr[i + minLen] = word1[i];
+            }
+        }
+        else if (word2.length() > minLen) {
+            for (int i = minLen; i < word2.length(); i++) {
+                resultStr[i + minLen] = word2[i];
+            }
+        }
+
+        std::string result(resultStr);
+        delete[] resultStr;
+        return result;
+    }
+};
