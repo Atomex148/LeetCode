ffdd40329fb28c7f54908a4884cc28a5186c27e1
Atomex148 <unclebenis148@gmail.com>
2024-07-29 23:19:14 +0300

    Time: 0 ms (100.00%), Space: 8.5 MB (13.56%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        unordered_map<char, int> jewelsMap;
+        unordered_set<char> jewelsMap;
         int counter = 0;
 
         for (char ch : jewels) {
-            jewelsMap[ch] = 0;
+            jewelsMap.insert(ch);
         }
 
         for (char ch : stones) {
