48d71e02b27ad6799d1eea3344d6c4fbb5c7c6be
Atomex148 <unclebenis148@gmail.com>
2024-09-03 09:14:09 +0300

    Time: 80 ms (93.68%), Space: 77.1 MB (9.10%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        int index = 0;
         const int emount_of_students = chalk.size();
         if (emount_of_students == 1) return 0;
         long chalk_sum = 0;
-        for (int i = 0; i < chalk.size(); i++) {
-            chalk_sum += chalk[i];
-        }
-
+        for (int i = 0; i < emount_of_students; i++) chalk_sum += chalk[i];
         k = k % chalk_sum;
 
-        while (true) {
-            index = index % emount_of_students;
-            if (k >= chalk[index]) k -= chalk[index];
-            else break;
-            index++;    
+        for (int i = 0; i < emount_of_students; i++) {
+            if (k < chalk[i]) return i;
+            k -= chalk[i];
         }
-        return index;
+
+        return 0;
     }
 };
